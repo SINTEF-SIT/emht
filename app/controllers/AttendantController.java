ca59c067a1beb14f2ae2844bf86af856020f06aa
mythern <myth@overflow.no>
2015-06-10 12:53:49 +0200

    Add authentication annotations for all /attendants controllers

--- app/controllers/AttendantController.java
+++ app/controllers/AttendantController.java
@@ -1,40 +1,58 @@
 package controllers;
 
+import controllers.auth.Authorization;
 import models.AlarmAttendant;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Security;
 
 public class AttendantController extends Controller {
 
 	static Form<AlarmAttendant> attendantForm = Form.form(AlarmAttendant.class);
 
+	/**
+	 * Main controller of the /attendants endpoint
+	 * @return A Result object containing the HTTP response
+	 */
+	@Security.Authenticated(Authorization.Authorized.class)
 	public static Result index() {
 		return redirect(controllers.routes.AttendantController.attendants());
-		//return ok(index.render("Your new application is ready."));
 	}
 
-	public static Result attendants(){
-		return ok(
-			views.html.attendantAdmin.render(AlarmAttendant.all(), attendantForm)
-		);
+	/**
+	 * Listing controller of the /attendants endpoint
+	 * @return A Result object containing the HTTP response
+	 */
+	@Security.Authenticated(Authorization.Authorized.class)
+	public static Result attendants() {
+		return ok(views.html.attendantAdmin.render(AlarmAttendant.all(), attendantForm));
 	}
 
-	public static Result newAttendant(){
-		Form<AlarmAttendant> filledForm = attendantForm.bindFromRequest(); // create a new form with the request data
-		if(filledForm.hasErrors()) {
-			return badRequest(
-				views.html.attendantAdmin.render(AlarmAttendant.all(), filledForm)
-			);
+	/**
+	 * New attendant creation controller of the /attendants endpoint
+	 * @return A Result object containing the HTTP response
+	 */
+	@Security.Authenticated(Authorization.Authorized.class)
+	public static Result newAttendant() {
+
+		Form<AlarmAttendant> filledForm = attendantForm.bindFromRequest();
+
+		if (filledForm.hasErrors()) {
+			return badRequest(views.html.attendantAdmin.render(AlarmAttendant.all(), filledForm));
 		} else {
-			AlarmAttendant.create(filledForm.get());//
+			AlarmAttendant.create(filledForm.get());
 			return redirect(controllers.routes.AttendantController.attendants());
 		}
 	}
 
-	public static Result deleteAttendant(Long id){
+	/**
+	 * Attendant deletion controller of the /attendants endpoint
+	 * @return A Result object containing the HTTP response
+	 */
+	@Security.Authenticated(Authorization.Authorized.class)
+	public static Result deleteAttendant(Long id) {
 		AlarmAttendant.delete(id);
 		return redirect(controllers.routes.AttendantController.attendants());
 	}
-
 }
