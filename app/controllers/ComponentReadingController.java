0c4a956ef5cec62dd6296a209bdc311026c7f5f8
mythern <myth@overflow.no>
2015-06-29 14:57:07 +0200

    Clean out sensorReading template

--- app/controllers/ComponentReadingController.java
+++ app/controllers/ComponentReadingController.java
@@ -9,9 +9,7 @@ import play.mvc.Result;
 public class ComponentReadingController extends Controller {
 
 	public static Result getReadingsPerType(String readingType) {
-		List<ComponentReading> l = ComponentReading.getReadingsOfType(readingType);
-		if (l.isEmpty()) return badRequest();
-		else return ok(views.html.sensorReading.render(l));
+		return ok(views.html.sensorReading.render());
 	}
 
 }
